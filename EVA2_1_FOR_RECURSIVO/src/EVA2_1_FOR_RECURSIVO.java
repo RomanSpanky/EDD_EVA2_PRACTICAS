799e82fd8adb4de0e6af9c7d750cd2cb2af7b6f7
Roman Najera <82838216+RomanSpanky@users.noreply.github.com>
2021-12-21 22:24:57 -0700

    Add files via upload

---
+++ EVA2_1_FOR_RECURSIVO/src/EVA2_1_FOR_RECURSIVO.java
@@ -0,0 +1,14 @@
+
+public class EVA2_1_FOR_RECURSIVO {
+
+    public static void main(String[] args) {
+        recursivo();
+    }
+
+    public static void recursivo() {//Método recursivo
+        System.out.println("Hola mundo!");
+        recursivo();
+
+    }
+
+}
